a93870a16cf0024293f71ebc491017a4be20ebba
octavionancul <octavionancul@gmail.com>
2018-04-22 20:35:28 -0300

    Initial commit

---
+++ app/src/main/java/cl/octavionancul/flash/CurrentUser.java
@@ -0,0 +1,13 @@
+package cl.octavionancul.flash;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class CurrentUser {
+
+    private FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+
+    public FirebaseUser getCurrentUser() {
+        return currentUser;
+    }
+}
